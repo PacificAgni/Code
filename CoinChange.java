a00445e58abfa0b3db8539385f8d2efe09cc324a
Prashant Agnihotri <140973461+PacificAgni@users.noreply.github.com>
2024-01-08 22:47:48 +0530

    Create CoinChange.java
    You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
    
    Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
    
    You may assume that you have an infinite number of each kind of coin.

---
+++ CoinChange.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        if(amount==0)
+        return 0;
+        int[] dp=new int[amount+1];
+        Arrays.fill(dp,Integer.MAX_VALUE-1);//to avoid overflow
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
+        {
+            for(int coin:coins)
+            {
+                if(coin<=i && dp[i-coin]!=Integer.MAX_VALUE-1)
+                {
+                    dp[i]=Math.min(dp[i],1+dp[i-coin]);
+                }
+            }
+        }
+        return dp[amount]==Integer.MAX_VALUE-1?-1:dp[amount];
+    }
+}
