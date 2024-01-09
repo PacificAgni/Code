72974f6e62132a567fb84d86e5e65bcf2881117b
Prashant Agnihotri <140973461+PacificAgni@users.noreply.github.com>
2024-01-09 20:01:48 +0530

    Create DeleteBST.java
    Given a root node reference of a BST and a key, delete the node with the given key in the BST. Return the root node reference (possibly updated) of the BST.
    
    Basically, the deletion can be divided into two stages:
    
    Search for a node to remove.
    If the node is found, delete the node.

---
+++ DeleteBST.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if(root == null)
+        return null;
+        if(key<root.val)//The key might be in the left SubTree
+            root.left=deleteNode(root.left,key);
+        else if(key> root.val)//The key might be in the right SubTree
+            root.right=deleteNode(root.right,key);
+        else//The key is found
+        {
+            if(root.left==null)
+            return root.right;
+            else if(root.right==null)
+            return root.left;
+
+            root.val=findMin(root.right).val;
+            root.right=deleteNode(root.right,root.val);
+        }
+        return root;
+    }
+    private TreeNode findMin(TreeNode node)
+    {
+        while(node.left!=null)
+        node=node.left;
+        return node;
+    }
+}
