1270c82eb9b8df86c855c7d6c78b07535d9e141c
Prashant Agnihotri <140973461+PacificAgni@users.noreply.github.com>
2024-01-10 22:30:43 +0530

    Create LeftSideViewBST.java

---
+++ LeftSideViewBST.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+         List<Integer> ans = new ArrayList<>();
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelSize = queue.size();
+            TreeNode rightMost = null;
+
+            for (int i = 0; i < levelSize; i++) {
+                TreeNode current = queue.poll();
+                if(i==0)
+                System.out.println(current.val);
+                // rightMost = current; // Update rightMost at each level
+
+                if (current.left != null) {
+                    queue.offer(current.left);
+                }
+
+                if (current.right != null) {
+                    queue.offer(current.right);
+                }
+            }
+
+           // ans.add(rightMost.val);
+        }
+
+        return ans;
+    }
+}
